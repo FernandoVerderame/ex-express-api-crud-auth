86d87a8cbde6a3b265404da84d9858d04e3e7fb5
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-10 16:37:25 +0200

    created validation token

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+// Importo JWT
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const validationToken = (req, res, next) => {
+
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+        throw new Error("Token non fornito");
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+        if (err) {
+            throw new Error("Token non valido");
+        }
+        req.user = data;
+        next();
+    });
+
+}
+
+module.exports = validationToken;
