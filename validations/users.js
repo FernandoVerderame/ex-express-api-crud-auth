aaa03816aac9af2fc72b659b765498112e94bbae
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-10 15:02:06 +0200

    added register validations

---
+++ validations/users.js
@@ -0,0 +1,60 @@
+// Importo PrismaClient
+const { PrismaClient } = require("@prisma/client");
+const { options } = require("../routers/auth");
+
+// Inizializzo Prisma
+const prisma = new PrismaClient();
+
+const registerBody = {
+    email: {
+        in: ["body"],
+        notEmpty: {
+            errorMessage: "L'eamil è obbligatoria!",
+            bail: true
+        },
+        isEmail: {
+            errorMessage: "Email non valida!"
+        },
+        custom: {
+            options: async (value) => {
+                const user = await prisma.user.findUnique({
+                    where: { email: value }
+                });
+
+                if (user) {
+                    throw new Error("Esiste già un utente con questa mail!");
+                }
+                return true;
+            }
+        }
+    },
+    name: {
+        in: ["body"],
+        isString: {
+            errorMessage: "Il nome deve essere una stringa!"
+        },
+        isLength: {
+            errorMessage: 'Name deve essere di almeno 3 caratteri',
+            options: { min: 3 }
+        }
+    },
+    password: {
+        in: ["body"],
+        notEmpty: {
+            errorMessage: "La password è obbligatoria!",
+            bail: true
+        },
+        isString: {
+            errorMessage: "Il nome deve essere una stringa!",
+            bail: true
+        },
+        isLength: {
+            errorMessage: "La password deve essere di almeno 8 caratteri",
+            options: { min: 8 }
+        }
+    }
+}
+
+module.exports = {
+    registerBody
+}
